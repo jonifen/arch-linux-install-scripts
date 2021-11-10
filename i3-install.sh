2280a7af3472c024766ce054007afe0cb9cd17f3
Jon Cain <jonifen@users.noreply.github.com>
2021-11-10 23:41:45 +0000

    Update i3-install.sh

--- i3-install.sh
+++ i3-install.sh
@@ -1,6 +1,6 @@
 # Installs i3 (requires that the base-install has already been run)
 
-pacman -S xorg i3 lxappearance --noconfirm
+pacman -S xorg i3 lxappearance xorg-xinit --noconfirm
 
 # Install other utils (web browser etc.)
 pacman -S firefox pcmanfm terminator dmenu nitrogen --noconfirm
@@ -8,3 +8,7 @@ pacman -S firefox pcmanfm terminator dmenu nitrogen --noconfirm
 # Install audio related utils
 pacman -S alsa-utils pulseaudio pavucontrol --noconfirm
 
+cp /etc/X11/xinit/xinitrc ~/.xinitrc
+
+echo "Now edit ~/.xinitrc to comment out any exec commands at the bottom and add the following:"
+echo " exec i3"
